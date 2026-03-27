49edf852154b60031b25e78d739ceea71a74bf76
KazedDev <beekwearl@gmail.com>
2026-03-27 14:57:28 +0100

    PlatformSpoofer.plugin.js

--- PlatformSpoofer.plugin.js
+++ PlatformSpoofer.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name PlatformSpoofer
- * @version 1.9.0
+ * @version 1.9.3
  * @author Kazed
  * @description Spoof what platform or device you're on
  * @authorLink https://github.com/KazedDev
